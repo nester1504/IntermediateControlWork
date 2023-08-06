bdf82460cde0665d613244865b23af9d932d7f9e
Alexandr Nesterenko <nesteronok@gmail.com>
2023-08-07 00:15:29 +0600

    Добавил комментарии

--- noteapp.py
+++ noteapp.py
@@ -1,8 +1,9 @@
 import json
 from datetime import datetime
 
-# Файл для хранения заметок в формате JSON
 NOTES_FILE = "notes.json"
+
+# Функция для загрузки заметок из файла
 
 
 def load_notes():
@@ -10,13 +11,17 @@ def load_notes():
         with open(NOTES_FILE, "r") as file:
             notes = json.load(file)
     except FileNotFoundError:
-        notes = []
+        notes = []  # Если файл не найден, начинаем с пустого списка заметок
     return notes
+
+# Функция для сохранения заметок в файл
 
 
 def save_notes(notes):
     with open(NOTES_FILE, "w") as file:
         json.dump(notes, file, indent=4)
+
+# Функция для добавления новой заметки
 
 
 def add_note():
@@ -28,17 +33,23 @@ def add_note():
         "message": msg,
         "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
     }
-    notes.append(note)
-    save_notes(notes)
+    notes.append(note)  # Добавляем новую заметку в список
+    save_notes(notes)    # Сохраняем список заметок в файл
     print("Заметка успешно добавлена!")
 
+# Функция для вывода всех заметок
 
-def show_notes():
-    for note in notes:
+
+def show_notes(notes_to_show=None):
+    if not notes_to_show:
+        notes_to_show = notes
+    for note in notes_to_show:
         print(
             f"ID: {note['id']}, Заголовок: {note['title']}, Дата создания: {note['created_at']}")
         print(note["message"])
         print("=" * 30)
+
+# Функция для редактирования заметки
 
 
 def edit_note():
@@ -55,6 +66,8 @@ def edit_note():
             return
     print("Заметка с таким ID не найдена.")
 
+# Функция для удаления заметки
+
 
 def delete_note():
     note_id = int(input("Введите ID заметки, которую хотите удалить: "))
@@ -66,6 +79,8 @@ def delete_note():
             return
     print("Заметка с таким ID не найдена.")
 
+# Функция для фильтрации заметок по дате
+
 
 def filter_notes_by_date():
     date_str = input(
@@ -74,15 +89,20 @@ def filter_notes_by_date():
         filter_date = datetime.strptime(date_str, "%Y-%m-%d")
         filtered_notes = [note for note in notes if datetime.strptime(
             note["created_at"], "%Y-%m-%d %H:%M:%S").date() == filter_date.date()]
-        print("Список заметок за указанную дату:")
-        show_notes(filtered_notes)
+        if filtered_notes:
+            print("Список заметок за указанную дату:")
+            show_notes(filtered_notes)
+        else:
+            print("Заметки за указанную дату не найдены.")
     except ValueError:
         print("Некорректный формат даты. Попробуйте ещё раз.")
+
+# Основная функция
 
 
 def main():
     global notes
-    notes = load_notes()
+    notes = load_notes()  # Загружаем заметки из файла при запуске программы
 
     while True:
         print("\n===== Команды =====")
